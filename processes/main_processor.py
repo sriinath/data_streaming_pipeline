b7381773c3c7fefe9965bef7386973ec050ca7d8
Srinath Karthikeyan <sriinathk@gmail.com>
2020-07-11 21:29:18 +0530

    Consume message and push to subscribers

--- processes/main_processor.py
+++ processes/main_processor.py
@@ -1,13 +1,11 @@
 import json
+import requests
 from time import sleep
 
 from .consumer_manager import KafkaConsumerManager
+from .redis_utils import DEFAULT_REDIS
 from workers.kafka_worker.producer import default_producer
-from constants import HEARTBEAT_INTERVAL, MAX_MESSAGES_PER_CONSUMER, MAX_CONSUMERS
-
-def default_processor(message):
-    print('received message from topic', message.topic)
-    print('consuming message for internal process', message)
+from constants import HEARTBEAT_INTERVAL, MAX_MESSAGES_PER_CONSUMER, MAX_CONSUMERS, TOPIC_PREFIX
 
 def heart_beat(consumer_manager):
     assert isinstance(consumer_manager, KafkaConsumerManager), 'consumer manager must be instance of KafkaConsumerManager'
@@ -22,6 +20,38 @@ def heart_beat(consumer_manager):
         except Exception as exc:
             print(exc)
         sleep(HEARTBEAT_INTERVAL)
+
+def default_processor(records):
+    try:
+        for message in records:
+            topic = str(message.topic)
+            key = str(message.key)
+            value = str(message.value)
+            if topic and (key or value):
+                topic_key = '{}:{}'.format(TOPIC_PREFIX, topic)
+                subscription_info = DEFAULT_REDIS.get_hash(
+                    topic_key
+                )
+                message_data = dict(
+                    topic=topic,
+                    key=key,
+                    value=value
+                )
+                for data in subscription_info.values():
+                    data = json.loads(data)
+                    url = data.get('url', '')
+                    headers = data.get('headers', {})
+                    method = data.get('method', 'post')
+                    req = requests.request(
+                        method, url, headers=headers, json=message_data
+                    )
+                    if req.status_code != 200:
+                        print('Failed to push message to url: ', url)
+                        print(req)
+                    else:
+                        print('successfully pushed')
+    except Exception as exc:
+        print(exc)
 
 print('Starting the worker consumer manager')
 default_consumer = KafkaConsumerManager(
