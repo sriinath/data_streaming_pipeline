2a8d151485bf058f50babc0ee9dcc22f0bc5f472
Srinath Karthikeyan <sriinathk@gmail.com>
2021-01-11 20:01:30 +0530

    Health check API Added (#8)

--- routes/Ping.py
+++ routes/Ping.py
@@ -1,6 +1,44 @@
 import falcon
+from constants import HEALTHCHECK_HASHKEY, META_INFO_HASHKEY
+from services.redis import redis_cli
+
+def convert_bytestring_keyvalue(data):
+    """
+    convert {b'':b''} to {'': ''}
+    """
+    if data is not None:
+        return {
+            key.decode(): val.decode() for key, val in data.items()
+        }
+    return {}
 
 class Ping:
     def on_get(self, request, response):
         response.status = falcon.HTTP_200
         response.body = 'THE NORTH REMEMBERS...'
+
+class ConsumerMeta:
+    def on_get(self, request, response):
+        response.status = falcon.HTTP_200
+        response.body = 'THE NORTH REMEMBERS...'
+
+class HealthCheck:
+    def on_get(self, request, response):
+        health_check_info = convert_bytestring_keyvalue(
+            redis_cli.get_all_hash_key_value(HEALTHCHECK_HASHKEY)
+        )
+        healthy_count = 0
+        unhealthy_count = 0
+
+        for info in health_check_info.values():
+            if int(info) == 1:
+                healthy_count += 1
+            else:
+                unhealthy_count += 1
+        
+        healthy_data = dict(
+            healthy_count=healthy_count,
+            unhealthy_count=unhealthy_count
+        )
+        response.status = falcon.HTTP_200
+        response.body = healthy_data
--- workers/kafka_worker/consumer.py
+++ workers/kafka_worker/consumer.py
@@ -1,21 +1,28 @@
 from kafka import KafkaConsumer
 from time import sleep
+import json
 
-from constants import MAX_POLL_DELAY, MIN_POLL_DELAY, MAX_RECORDS_PER_POLL
+from constants import MAX_POLL_DELAY, MIN_POLL_DELAY, MAX_RECORDS_PER_POLL, \
+    HEALTHCHECK_HASHKEY, META_INFO_HASHKEY
+from services.redis import redis_cli
 
 class Consumer:
     __flight_messages = dict()
     __consumers_count = 0
 
     def __init__(self, *args, **kwargs):
-        print('Topics listened by the consumers:', *args)
+        self.topics = args
+        self.consumer_id = kwargs.pop('consumer_id', Consumer.__consumers_count + 1)
+        self.manager_id = kwargs.pop('manager_id', '')
         self.__consumer = KafkaConsumer(*args, **kwargs)
         self.__enable_polling = True
         self.__is_active = True
         self.__poll_delay = MIN_POLL_DELAY
         self.__group_id = kwargs.get('group_id', None)
         self.__name = 'Consumer_{}'.format(Consumer.get_consumer_count())
+        self.processed_images = 0
         Consumer.increment_consumer_count()
+        print('Topics listened by the consumers:', self.topics)
         if self.__group_id and self.__group_id not in Consumer.__flight_messages:
             Consumer.__flight_messages.update({ self.__group_id: 0 })
 
@@ -61,6 +68,21 @@ class Consumer:
             process_fn(message)
         self.close_consumer()
 
+    def set_alive(self, is_alive):
+        redis_cli.set_multi_value(HEALTHCHECK_HASHKEY, '{}:{}'.format(self.manager_id, self.consumer_id), 1 if is_alive else 0)
+
+    def log_consumer_meta(self):
+        consumer_meta = dict(
+            messages_count=self.processed_images,
+            topics=self.topics,
+            group_id=self.__group_id,
+            name=self.__name
+        )
+        redis_cli.set_multi_value(
+            META_INFO_HASHKEY, '{}:{}'.format(self.manager_id, self.consumer_id),
+            json.loads(consumer_meta)
+        )
+
     def __poll(self, timeout_ms=0, max_records=MAX_RECORDS_PER_POLL):
         while self.__enable_polling:
             print('{} Listening to messages...'.format(self.__name))
@@ -86,12 +108,18 @@ class Consumer:
                         self.__poll_delay = delay
                     else:
                         self.__poll_delay = MAX_POLL_DELAY
+
+                self.processed_images += len(records)
+                self.log_consumer_meta()
+                self.set_alive(False)
+                self.log_consumer_meta()
                 sleep(self.__poll_delay)
             except AssertionError as assertion_exc:
                 self.stop_polling()
                 print(assertion_exc)
 
         print('returning None')
+        self.set_alive(False)
         return None
 
     def poll_topics(self, process_fn, timeout_ms=0, max_records=MAX_RECORDS_PER_POLL):
